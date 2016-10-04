f14372ee899489e926f32790575e8373c6359fd7
nbkhope <ryumemaru@yahoo.com>
2016-10-03 20:58:11 -0700

    Add Resources component and add this.props.children to App file so it shows nested routes

--- src/components/app.js
+++ src/components/app.js
@@ -7,6 +7,8 @@ export default class App extends Component {
     return (
       <div>
         <Header />
+        
+        {this.props.children}
       </div>
     );
   }
---
+++ src/components/resources.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <div>
+      <h2>Welcome to the secret page.</h2>
+
+      <p>{"Congratulations, you've made it!"}</p>
+    </div>
+  );
+};
