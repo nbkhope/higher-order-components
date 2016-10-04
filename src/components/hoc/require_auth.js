a217be6005aff047d0454bdebbbe04bae639cdba
nbkhope <ryumemaru@yahoo.com>
2016-10-04 13:18:23 -0700

    Update require auth higher order component to redirect to root / if user is not authenticated (also works if you click sign out while on the protected page)

--- src/components/hoc/require_auth.js
+++ src/components/hoc/require_auth.js
@@ -1,13 +1,38 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
 
 export default function(ComposedComponent) {
   class Authentication extends Component {
+    static contextTypes = {
+      router: PropTypes.object
+    }
+
+    componentWillMount() {
+      const { authenticated } = this.props;
+
+      if (!authenticated) {
+        this.context.router.push('/');
+      }
+    }
+
+    // Need this so when you click sign out while on the Resources
+    // page, you will be redirected out.
+    componentWillUpdate(nextProps) {
+      if (!nextProps.authenticated) {
+        this.context.router.push('/');
+      }
+    }
+
     render() {
       return <ComposedComponent {...this.props} />
     }
   }
 
-  return Authentication;
+  function mapStateToProps(state) {
+    return { authenticated: state.authenticated };
+  }
+
+  return connect(mapStateToProps)(Authentication);
 }
 
 // usage:
